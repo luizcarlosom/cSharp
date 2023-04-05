a72025d9fb021d9a0f8f220bad6c81df05872bc4
Luttarios-069 <117871011+luizcarlosom@users.noreply.github.com>
2023-04-05 13:34:29 -0300

    Initial commit

---
+++ tutorialCsharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// Console.WriteLine("Hello World!");
+
+//Console.Write("Congratulations!");
+//Console.Write(" ");
+//Console.Write("You wrote your first lines of code!");
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
