21f0196584c5eb61f655b529e12f86b8e5cae1e7
Luttarios-069 <117871011+luizcarlosom@users.noreply.github.com>
2023-04-14 13:47:47 -0300

    Finalização do tutorial .NET

---
+++ tutorialCsharp002/Program.cs
@@ -0,0 +1,28 @@
+﻿using Humanizer;
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("case".ToQuantity(0));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    var culture = System.Globalization.CultureInfo.CurrentCulture;
+
+    Console.WriteLine(culture);
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+    Console.WriteLine(2023.ToWords());
+    Console.WriteLine(20.Ordinalize());
+
+}
